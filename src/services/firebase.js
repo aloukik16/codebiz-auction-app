4442c6794792da534b97e8e929d887e17ac5998d
Aloukik Agrawal <150384385+aloukik16@users.noreply.github.com>
2026-03-16 23:26:24 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+}
 
 
 const app = initializeApp(firebaseConfig);
