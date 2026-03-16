4a636e878048fa4d5847c2e01f609a4e83285ddc
Aloukik Agrawal <150384385+aloukik16@users.noreply.github.com>
2026-03-16 23:18:46 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzHQEvH7g64STAKgQPX4Z_n11ccci0CpU",
-  authDomain: "codebidz-auction-eca65.firebaseapp.com",
-  projectId: "codebidz-auction-eca65",
-  storageBucket: "codebidz-auction-eca65.firebasestorage.app",
-  messagingSenderId: "715077557070",
-  appId: "1:715077557070:web:3bafa5bfdc082ab2f69c1f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "5",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
